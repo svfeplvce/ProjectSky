eed9a5b9a1664891f9b590f3b59460a958cde284
svfeplvce <asrielknight28@gmail.com>
2023-09-03 22:55:31 -0500

    evo editor ALMOST DONE

---
+++ Core/MethodArgType.cs
@@ -0,0 +1,30 @@
+﻿using ProjectSky.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace ProjectSky.Core
+{
+    public class MethodArgType : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (values.Length == 2 && values[0] is int methodID && values[1] is ObservableCollection<EvoMethod> evoMethods)
+            {
+                EvoMethod selectedMethod = evoMethods.FirstOrDefault(m => m.MethodID == methodID);
+                return selectedMethod;
+            }
+            return null;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- ViewModels/PokeEditorViewModel.cs
+++ ViewModels/PokeEditorViewModel.cs
@@ -84,75 +84,33 @@ namespace ProjectSky.ViewModels
             }
         }
 
+        private Core.RelayCommand _addEvoCommand;
+        public Core.RelayCommand AddEvoCommand
+        {
+            get
+            {
+                if (_addEvoCommand == null)
+                {
+                    _addEvoCommand = new Core.RelayCommand(o => { AddEvo(); }, o => true);
+                }
+                return _addEvoCommand;
+            }
+        }
+
+        private Core.RelayCommand _removeEvoCommand;
+        public Core.RelayCommand RemoveEvoCommand
+        {
+            get
+            {
+                if (_removeEvoCommand == null)
+                {
+                    _removeEvoCommand = new Core.RelayCommand(o => { RemoveEvo(); }, o => true);
+                }
+                return _removeEvoCommand;
+            }
+        }
+
         public string PokeName { get; private set; }
-        public readonly List<EvoMethod> evoMethods = new List<EvoMethod> {
-            new EvoMethod{ Method="None", MethodID=0, AdditionalArgs=false, UsesLevel=false, ArgType="None" },
-            new EvoMethod{ Method="Level Up With High Friendship", MethodID=1, AdditionalArgs=false, UsesLevel=false, ArgType="None" },
-            new EvoMethod{ Method="Level Up With High Friendship At Daytime", MethodID=2, AdditionalArgs=false, UsesLevel=false, ArgType="None" },
-            new EvoMethod{ Method="Level Up With High Friendship At Nighttime", MethodID=3, AdditionalArgs=false, UsesLevel=false, ArgType="None" },
-            new EvoMethod{ Method="Level Up", MethodID=4, AdditionalArgs=false, UsesLevel=true, ArgType="None" },
-            new EvoMethod{ Method="Trade", MethodID=5, AdditionalArgs=false, UsesLevel=false, ArgType="None" },
-            new EvoMethod{ Method="Trade With Held Item", MethodID=6, AdditionalArgs=true, UsesLevel=false, ArgType="Item" },
-            new EvoMethod{ Method="Trade (Shelmet/Karrablast)", MethodID=7, AdditionalArgs=false, UsesLevel=false, ArgType="None" },
-            new EvoMethod{ Method="Use Item", MethodID=8, AdditionalArgs=true, UsesLevel=false, ArgType="Item" },
-            new EvoMethod{ Method="Level Up While ATK > DEF", MethodID=9, AdditionalArgs=false, UsesLevel=true, ArgType="None" },
-            new EvoMethod{ Method="Level Up While ATK = DEF", MethodID=10, AdditionalArgs=false, UsesLevel=true, ArgType="None" },
-            new EvoMethod{ Method="Level Up While ATK < DEF", MethodID=11, AdditionalArgs=false, UsesLevel=true, ArgType="None" },
-            new EvoMethod{ Method="Level Up With A Certain Personality Value", MethodID=12, AdditionalArgs=false, UsesLevel=true, ArgType="None" },
-            new EvoMethod{ Method="Level Up With A Certain Personality Value 2", MethodID=13, AdditionalArgs=false, UsesLevel=true, ArgType="None" },
-            new EvoMethod{ Method="Level Up (Ninjask)", MethodID=14, AdditionalArgs=false, UsesLevel=true, ArgType="None" },
-            new EvoMethod{ Method="Level Up (Shedinja)", MethodID=15, AdditionalArgs=false, UsesLevel=true, ArgType="None" },
-            new EvoMethod{ Method="Level Up With High Beauty", MethodID=16, AdditionalArgs=true, UsesLevel=false, ArgType="Stat" },
-            new EvoMethod{ Method="Use Item While Pokemon is Male", MethodID=17, AdditionalArgs=true, UsesLevel=false, ArgType="Item" },
-            new EvoMethod{ Method="Use Item While Pokemon is Female", MethodID=18, AdditionalArgs=true, UsesLevel=false, ArgType="Item" },
-            new EvoMethod{ Method="Level Up With Held Item At Daytime", MethodID=19, AdditionalArgs=true, UsesLevel=false, ArgType="Item" },
-            new EvoMethod{ Method="Level Up With Held Item At Nighttime", MethodID=20, AdditionalArgs=true, UsesLevel=false, ArgType="Item" },
-            new EvoMethod{ Method="Level Up While Knowing Move", MethodID=21, AdditionalArgs=true, UsesLevel=false, ArgType="Move" },
-            new EvoMethod{ Method="Level Up With Teammate", MethodID=22, AdditionalArgs=true, UsesLevel=false, ArgType="Species" },
-            new EvoMethod{ Method="Level Up While Pokemon is Male", MethodID=23, AdditionalArgs=false, UsesLevel=true, ArgType="None" },
-            new EvoMethod{ Method="Level Up While Pokemon is Female", MethodID=24, AdditionalArgs=false, UsesLevel=true, ArgType="None" },
-            new EvoMethod{ Method="Level Up Near Electric Rock", MethodID=25, AdditionalArgs=false, UsesLevel=false, ArgType="None" },
-            new EvoMethod{ Method="Level Up Near Mossy Rock", MethodID=26, AdditionalArgs=false, UsesLevel=false, ArgType="None" },
-            new EvoMethod{ Method="Level Up Near Cold Rock", MethodID=27, AdditionalArgs=false, UsesLevel=false, ArgType="None" },
-            new EvoMethod{ Method="Level Up While Console is Inverted", MethodID=28, AdditionalArgs=false, UsesLevel=false, ArgType="None" },
-            new EvoMethod{ Method="Level Up With High Affection And A Move Of A Specific Type", MethodID=29, AdditionalArgs=true, UsesLevel=false, ArgType="Type" },
-            new EvoMethod{ Method="Level Up While Knowing A Move Of A Specific Type", MethodID=30, AdditionalArgs=true, UsesLevel=false, ArgType="Type" },
-            new EvoMethod{ Method="Level Up During Rainy Weather", MethodID=31, AdditionalArgs=false, UsesLevel=true, ArgType="None" },
-            new EvoMethod{ Method="Level Up During Morning", MethodID=32, AdditionalArgs=false, UsesLevel=true, ArgType="None" },
-            new EvoMethod{ Method="Level Up During Night", MethodID=33, AdditionalArgs=false, UsesLevel=true, ArgType="None" },
-            new EvoMethod{ Method="Level Up While Pokemon is Female Form", MethodID=34, AdditionalArgs=false, UsesLevel=true, ArgType="None" },
-            new EvoMethod{ Method="UNUSED", MethodID=35, AdditionalArgs=false, UsesLevel=false, ArgType="None" },
-            new EvoMethod{ Method="Level Up In Specific Version", MethodID=36, AdditionalArgs=true, UsesLevel=false, ArgType="Misc" },
-            new EvoMethod{ Method="Level Up In Specific Version During Daytime", MethodID=37, AdditionalArgs=true, UsesLevel=false, ArgType="Misc" },
-            new EvoMethod{ Method="Level Up In Specific Version During Nighttime", MethodID=38, AdditionalArgs=true, UsesLevel=false, ArgType="Misc" },
-            new EvoMethod{ Method="Level Up At A Summit", MethodID=39, AdditionalArgs=false, UsesLevel=true, ArgType="None" },
-            new EvoMethod{ Method="Level Up At Dusk", MethodID=40, AdditionalArgs=false, UsesLevel=true, ArgType="None" },
-            new EvoMethod{ Method="Level Up In Ultra Wormhole", MethodID=41, AdditionalArgs=false, UsesLevel=true, ArgType="None" },
-            new EvoMethod{ Method="Use Item In Ultra Wormhole", MethodID=42, AdditionalArgs=true, UsesLevel=false, ArgType="Item" },
-            new EvoMethod{ Method="Get x Crits In Battle", MethodID=43, AdditionalArgs=true, UsesLevel=false, ArgType="Misc" },
-            new EvoMethod{ Method="Lose HP In Battle", MethodID=44, AdditionalArgs=true, UsesLevel=false, ArgType="Misc" },
-            new EvoMethod{ Method="Spin", MethodID=45, AdditionalArgs=false, UsesLevel=false, ArgType="None" },
-            new EvoMethod{ Method="Level Up With Amped Nature", MethodID=46, AdditionalArgs=false, UsesLevel=true, ArgType="None" },
-            new EvoMethod{ Method="Level Up With LowKey Nature", MethodID=47, AdditionalArgs=false, UsesLevel=true, ArgType="None" },
-            new EvoMethod{ Method="Tower Of Darkness", MethodID=48, AdditionalArgs=false, UsesLevel=false, ArgType="None" },
-            new EvoMethod{ Method="Tower Of Waters", MethodID=49, AdditionalArgs=false, UsesLevel=false, ArgType="None" },
-            new EvoMethod{ Method="Level Up After Walking x Steps With In Lets Go Mode", MethodID=50, AdditionalArgs=true, UsesLevel=false, ArgType="Misc" },
-            new EvoMethod{ Method="Level Up In Union Circle", MethodID=51, AdditionalArgs=false, UsesLevel=false, ArgType="None" },
-            new EvoMethod{ Method="Level Up In Battle With A Certain Personality Value", MethodID=52, AdditionalArgs=false, UsesLevel=false, ArgType="None" },
-            new EvoMethod{ Method="Level Up In Battle With A Certain Personality Value 2", MethodID=53, AdditionalArgs=false, UsesLevel=false, ArgType="None" },
-            new EvoMethod{ Method="Level Up After Collecting 999 Gimmighoul Coins", MethodID=54, AdditionalArgs=false, UsesLevel=false, ArgType="None" },
-            new EvoMethod{ Method="Level Up After Defeating 3 Leader Bisharp", MethodID=55, AdditionalArgs=false, UsesLevel=false, ArgType="None" },
-            new EvoMethod{ Method="Level Up After Using A Move x Times", MethodID=56, AdditionalArgs=true, UsesLevel=false, ArgType="Misc" },
-            new EvoMethod{ Method="Level Up While Knowing A Move With A Certain Personality Value", MethodID=57, AdditionalArgs=true, UsesLevel=false, ArgType="Move" },
-            new EvoMethod{ Method="Level Up While Knowing A Move With A Certain Personality Value 2", MethodID=58, AdditionalArgs=true, UsesLevel=false, ArgType="Move" },
-            new EvoMethod{ Method="Level Up After Taking x Recoil Damage While Pokemon is Male", MethodID=59, AdditionalArgs=false, UsesLevel=false, ArgType="None" },
-            new EvoMethod{ Method="Level Up After Taking x Recoil Damage While Pokemon is Female", MethodID=60, AdditionalArgs=false, UsesLevel=false, ArgType="None" },
-            new EvoMethod{ Method="Hisui", MethodID=61, AdditionalArgs=false, UsesLevel=false, ArgType="None" },
-            new EvoMethod{ Method="Use Item During Full Moon", MethodID=90, AdditionalArgs=true, UsesLevel=false, ArgType="Item" },
-            new EvoMethod{ Method="Use Agile Style Moves", MethodID=91, AdditionalArgs=false, UsesLevel=false, ArgType="None" },
-            new EvoMethod{ Method="Use Strong Style Moves", MethodID=92, AdditionalArgs=false, UsesLevel=false, ArgType="None" }
-        };
-        private Dictionary<string, List<string>> evoArgs = new Dictionary<string, List<string>> { };
 
         private readonly List<string> tmList = new List<string> { "Take Down", "Charm", "Fake Tears", "Agility", "Mud Slap", "Scary Face", "Protect", "Fire Fang", "Thunder Fang", "Ice Fang", "Water Pulse", "Low Kick", "Acid Spray", "Acrobatics", "Struggle Bug", "Psybeam", "Confuse Ray", "Thief", "Disarming Voice", "Trailblaze", "Pounce", "Chilling Water", "Charge Beam", "Fire Spin", "Facade", "Poison Tail", "Aerial Ace", "Bulldoze", "Hex", "Snarl", "Metal Claw", "Swift", "Magical Leaf", "Icy Wind", "Mud Shot", "Rock Tomb", "Draining Kiss", "Flame Charge", "Low Sweep", "Air Cutter", "Stored Power", "Night Shade", "Fling", "Dragon Tail", "Venoshock", "Avalanche", "Endure", "Volt Switch", "Sunny Day", "Rain Dance", "Sandstorm", "Snowscape", "Smart Strike", "Psyshock", "Dig", "Bullet Seed", "False Swipe", "Brick Break", "Zen Headbutt", "U-Turn", "Shadow Claw", "Foul Play", "Psychic Fangs", "Bulk Up", "Air Slash", "Body Slam", "Fire Punch", "Thunder Punch", "Ice Punch", "Sleep Talk", "Seed Bomb", "Electro Ball", "Drain Punch", "Reflect", "Light Screen", "Rock Blast", "Waterfall", "Dragon Claw", "Dazzling Gleam", "Metronome", "Grass Knot", "Thunder Wave", "Poison Jab", "Stomping Tantrum", "Rest", "Rock Slide", "Taunt", "Swords Dance", "Body Press", "Spikes", "Toxic Spikes", "Imprison", "Flash Cannon", "Dark Pulse", "Leech Life", "Eerie Impulse", "Fly", "Skill Swap", "Iron Head", "Dragon Dance", "Power Gem", "Gunk Shot", "Substitute", "Iron Defense", "X-Scissor", "Drill Run", "Will-O-Wisp", "Crunch", "Trick", "Liquidation", "Giga Drain", "Aura Sphere", "Tailwind", "Shadow Ball", "Dragon Pulse", "Stealth Rock", "Hyper Voice", "Heat Wave", "Energy Ball", "Psychic", "Heavy Slam", "Encore", "Surf", "Ice Spinner", "Flamethrower", "Thunderbolt", "Play Rough", "Amnesia", "Calm Mind", "Helping Hand", "Pollen Puff", "Baton Pass", "Earth Power", "Reversal", "Ice Beam", "Electric Terrain", "Grassy Terrain", "Psychic Terrain", "Misty Terrain", "Nasty Plot", "Fire Blast", "Hydro Pump", "Blizzard", "Fire Pledge", "Water Pledge", "Grass Pledge", "Wild Charge", "Sludge Bomb", "Earthquake", "Stone Edge", "Phantom Force", "Giga Impact", "Blast Burn", "Hydro Cannon", "Frenzy Plant", "Outrage", "Overheat", "Focus Blast", "Leaf Storm", "Hurricane", "Trick Room", "Bug Buzz", "Hyper Beam", "Brave Bird", "Flare Blitz", "Thunder", "Close Combat", "Solar Beam", "Draco Meteor", "Steel Beam", "Tera Blast" };
 
@@ -280,7 +238,6 @@ namespace ProjectSky.ViewModels
                 PlibItems.Clear();
                 AbilitiesMenuItems.Clear();
                 ItemsMenuItems.Clear();
-                evoArgs.Clear();
                 TMMenuItems.Clear();
                 MovesMenuItems.Clear();
                 SelectorParamsToSend parameter = (SelectorParamsToSend)NavigationService.GetParameter<PokeEditorViewModel>();
@@ -349,14 +306,6 @@ namespace ProjectSky.ViewModels
             IsLoaded = true;
 
             OnPropertyChanged(nameof(CurrentSpecies));
-
-            List<int> ints = Enumerable.Range(0, 1000).ToList();
-
-            evoArgs.Add("Item", Items);
-            evoArgs.Add("Move", Moves);
-            evoArgs.Add("Species", SpeciesNames);
-            evoArgs.Add("Type", new List<string> { "Normal", "Fighting", "Flying", "Poison", "Ground", "Rock", "Bug", "Ghost", "Steel", "Fire", "Water", "Grass", "Electric", "Psychic", "Ice", "Dragon", "Dark", "Fairy" });
-            evoArgs.Add("Misc", ints.ConvertAll(x => x.ToString()));
         }
 
         private async Task FillItemList()
@@ -455,12 +404,12 @@ namespace ProjectSky.ViewModels
         
         private void AddEvo()
         {
-            CurrentSpecies.EntryInfo.evo_data.Add(new Personal.EvoDatum { level = 1, condition = 1, form = 0, species = 1 });
+            CurrentSpecies.EntryInfo.evo_data.Add(new Personal.EvoDatum { level = 1, condition = 4, form = 0, species = 1, parameter = 0 });
         }
 
         private void RemoveEvo()
         {
-            if (CurrentSpecies.EntryInfo.evo_data.Count != 1)
+            if (CurrentSpecies.EntryInfo.evo_data.Count != 0)
             {
                 CurrentSpecies.EntryInfo.evo_data.RemoveAt(CurrentSpecies.EntryInfo.evo_data.Count - 1);
             }
