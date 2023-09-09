42bbfc93019106fef02500d1cb13ee64f9b5d252
svfeplvce <asrielknight28@gmail.com>
2023-09-09 12:20:19 -0500

    oops

--- ViewModels/HomeViewModel.cs
+++ ViewModels/HomeViewModel.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace ProjectSky.ViewModels
 {
@@ -29,8 +30,12 @@ namespace ProjectSky.ViewModels
             NavigationService = navService;
             NavigateSelectCommand = new RelayCommand(o => { NavigationService.NavigateTo<SelectorViewModel>(); }, o => true);
             NavigateTrainerCommand = new RelayCommand(o => { NavigationService.NavigateTo<TrainerViewModel>(); }, o => true);
-            NavigateMoveCommand = new RelayCommand(o => { NavigationService.NavigateTo<MoveViewModel>(); }, o => true);
+            NavigateMoveCommand = new RelayCommand(o => { NotAdded(); }, o => true);
         }
 
+        private void NotAdded()
+        {
+            MessageBox.Show("Move editor is coming soon. If you would like to contribute, please feel free to download the source code and mess with it yourself.\n\nFollow phantomAnarch on GameBanana for updates on when it's coming.");
+        }
     }
 }
